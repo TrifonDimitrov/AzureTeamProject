1a06395398365142c91caecc84f2ef6fad082095
TrifonDimitrov <dimitrov1trifon@gmail.com>
2025-05-27 15:45:23 +0300

    Implement functionality to add a new town

--- Towns Project/Towns-main/towns.js
+++ Towns Project/Towns-main/towns.js
@@ -1,19 +1,28 @@
-$(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+$(document).ready(function () {
+  $("#btnAdd").click(addTown);
+});
+
+$(document).ready(function () {
+  $("#btnDelete").click(deleteTown);
 });
 
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+  let townName = $("#townName").val();
+  $("#townName").val("");
+  let removed = false;
+  for (let option of $("#towns option")) {
+    if (option.textContent == townName) {
+      removed = true;
+      option.remove();
+    }
+  }
+  if (removed) $("#result").text(townName + " deleted.");
+  else $("#result").text(townName + " not found.");
+}
+
+function addTown() {
+  let townName = $("#townNameForAdd").val();
+  $("#townNameForAdd").val("");
+  $("#towns").append($("<option>").text(townName));
+  $("#result").text(townName + " added.");
 }
