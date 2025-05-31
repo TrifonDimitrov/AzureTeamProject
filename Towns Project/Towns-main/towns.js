1860006baf2e1bd961e1a248737254aa80b16c31
Калоян Димитров <luckykaloyan@gmail.com>
2025-06-01 00:46:58 +0300

    Implemented functionality to shuffle the existing towns

--- Towns Project/Towns-main/towns.js
+++ Towns Project/Towns-main/towns.js
@@ -33,3 +33,12 @@ function showMessage(msg) {
     $("#result").hide("blind", {}, 500);
   }, 3000);
 }
+
+function shuffleTowns() { 
+  let towns = $('#towns option').toArray(); $('#towns').empty();
+shuffleArray(towns); 
+$('#towns').append(towns); $('#result').text("Towns shuffled."); 
+function shuffleArray(array) { for (var i = array.length - 1; i > 0; i--) { 
+  var j = Math.floor(Math.random() * (i + 1)); 
+  var oldElement = array[i]; array[i] = array[j]; array[j] = oldElement; } } 
+}
